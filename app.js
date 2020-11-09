d7898a173532acc86317d2a1d87ac9de8be8b656
roopamgarg <roopamg777@gmail.com>
2020-11-09 20:35:42 +0530

    added instructions

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+ * First we will build a virtual DOM 
+ * then we will try to parse our virtual DOM to create a real DOM
+ * 
+ * To Build A virtual DOM we need a function 
+ * that create a structure for each of our DOM element
+ * then we will use that function to crate our basic tags like div, h1 & p
+ * then with the help of our basic tags we try to make an complex component
+ * 
+ */
